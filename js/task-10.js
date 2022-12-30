1da61d48c7af6e81bddad5dfb0ab6af42130086a
Hanna <anna_kon@bigmir.net>
2022-12-30 21:41:28 +0200

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -16,27 +16,42 @@ function getRandomHexColor() {
 }
 
 
-function createBoxes(amount) {
-  const elementsToAdd = [];
+// function createBoxes(amount) {
+//   const elementsToAdd = [];
 
-  for (let i = 0; i < amount; i += 1) {
+//   for (let i = 0; i < amount; i += 1) {
     
     
+//     const newDiv = document.createElement('div');
+//     const size = `${30 + 10 * i}px`;
+    
+    
+//     newDiv.style.width = size;
+//     newDiv.style.height = size;
+//     newDiv.style.backgroundColor = getRandomHexColor();
+  
+//     elementsToAdd.push(newDiv);
+//   }
+//   return elementsToAdd;
+//  }
+
+function createBoxes(amount) {
+  return new Array(amount).fill('').map((item, i) => {
+    // створився новий масив з тієї к-ті елтів, яку ввели, а філ їх заповнив пустими строками, потім перебрали і на кожн ітерації додали нові елти
     const newDiv = document.createElement('div');
     const size = `${30 + 10 * i}px`;
-    
-    
+
     newDiv.style.width = size;
     newDiv.style.height = size;
     newDiv.style.backgroundColor = getRandomHexColor();
-  
-    // elementsToAdd.push(newDiv);
-  }
-  return elementsToAdd.push(newDiv);
- }
+    return newDiv;
+
+})
+}
   
 function onClickDestroyBtn() {
   boxesEl.innerHTML = '';
+  inputEl.value = '';
 }
 
 
@@ -44,3 +59,18 @@ function onClickCreateBtn() {
   let boxesToAdd = createBoxes(Number(inputEl.value));
   boxesEl.append(...boxesToAdd);
 }
+
+
+// Метод fill
+// const array1 = [1, 2, 3, 4];
+
+// Fill with 0 from position 2 until position 4
+// console.log(array1.fill(0, 2, 4));
+// expected output: Array [1, 2, 0, 0]
+
+// Fill with 5 from position 1
+// console.log(array1.fill(5, 1));
+// expected output: Array [1, 5, 5, 5]
+
+// console.log(array1.fill(6));
+// expected output: Array [6, 6, 6, 6]
